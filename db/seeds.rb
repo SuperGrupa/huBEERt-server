1021f57c32252ee5f972dd3a196c91f94a916589
Daniel Koza <dakolech@gmail.com>
2015-03-28 19:09:48 +0100

    generate controller and seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..10).each do |i|
+  User.create!(login:         Faker::Internet.user_name,
+               email:         Faker::Internet.free_email,
+               password:      Faker::Internet.password,
+               firstname:     Faker::Name.first_name,
+               lastname:      Faker::Name.last_name,
+               date_of_birth: rand(50.years).ago,
+               city:          Faker::Address.city)
+end
+
+User.create!(login:         'admin',
+             email:         'admin@hubeert.pl',
+             password:      'admin',
+             firstname:     'Ad',
+             lastname:      'Min',
+             city:          'Poznan')
