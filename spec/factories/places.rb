a830af085301b7e7b19f703776debc3f19f468f3
Daniel Koza <dakolech@gmail.com>
2015-04-13 11:34:16 +0200

    ci should not fail2

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :place do
     name "U Szwagra"
-    description "Najlepsza knajpka na świecie"
+    description "Najlepsza knajpka na swiecie"
     phone "444555666"
     email "wuja@szwagier.pl"
   end
