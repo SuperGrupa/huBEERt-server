c3b8b2244087b6773aaf4fe46619d92f1157973e
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2015-05-20 01:24:41 +0200

    Address POST test not pass - f***ing Factory Girl

--- app/controllers/api/v1/places_api/opening_hours.rb
+++ app/controllers/api/v1/places_api/opening_hours.rb
@@ -5,29 +5,32 @@ module API
                 include API::V1::Defaults
 
                 resource :places do
-                    # desc 'Return list of places'
-                    # get jbuilder: 'places/index' do
-                    #     @places = Place.includes(address: [street: {district: :city}])
-                    # end
-
                     desc 'Return opening hours of a given place'
                     get ':id/opening_hours', jbuilder: 'places/opening_hours/show' do
                         @opening_hours = Place.find(params[:id]).opening_hours
                     end
 
-                    # desc 'Create address'
-                    # params do
-                    #     requires :address, type: Hash, desc: 'address attributes' do
-                    #         requires :number, type: String, allow_blank: false
-                    #         requires :postcode, type: String, allow_blank: false
-                    #         requires :place_id, type: Integer
-                    #         requires :street_id, type: Integer
-                    #     end
-                    # end
-                    # post ':id/address' do
-                    #     Address.create params[:address].to_h
-                    # end
-                    #
+                    desc 'Create opening hours'
+                    params do
+                        requires :opening_hours, type: Array, desc: 'opening hours attributes' do
+                            requires :from, type: String, allow_blank: false
+                            requires :to, type: String, allow_blank: false
+                            requires :days, type: Array do
+                                requires :id, type: Integer
+                            end
+                        end
+                    end
+                    post ':id/opening_hours' do
+                        params[:opening_hours].each do |opening_hour|
+                            week_day_ids = opening_hour[:days].map do |day|
+                                day[:id]
+                            end
+                            
+                            Place.find(params[:id]).opening_hours.create!(from: opening_hour[:from],
+                                to: opening_hour[:to], week_day_ids: week_day_ids)
+                        end
+                    end
+
                     # desc 'Update place'
                     # params do
                     #     optional :name, type: String, allow_blank: false
