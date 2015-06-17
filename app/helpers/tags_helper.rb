67f0ae600414707a59d763d0e4ca6424c63848a2
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2015-06-17 13:13:06 +0200

    Tagging refactor part 2

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,32 +1,40 @@
 module TagsHelper
+    def find_and_delete_tag_from_place(place, previous_text)
+        tag = Tag.all.find { |tag| tag.name == previous_text }
+        unless tag.nil?
+            tag.place_ids.delete(place.id)
+            if tag.place_ids.empty?
+                tag.delete
+            else
+                tag.save
+            end
+            place.tags.delete(tag)
+        end
+    end
+    
+    def find_and_add_tag_to_place(place, current_text, weight)
+        tag = Tag.all.find { |tag| tag.name == current_text } || Tag.new
+        if tag.id.nil?
+            tag.name = current_text
+            tag.weight = weight
+            tag.searched = 0
+        end
+        tag.save
+        place.tags << tag
+    end
+
     def tagging(place_id, options = {})
         weight = options[:weight] || 1
         place = Place.find(place_id)
     
         unless options[:previous].nil?
             options[:previous].split(" ").each do |previous|
-                tag_previous = Tag.all.find { |tag| tag.name == previous }
-                unless tag_previous.nil?
-                    tag_previous.place_ids.delete(place.id)
-                    if tag_previous.place_ids.empty?
-                        tag_previous.delete
-                    else
-                        tag_previous.save
-                    end
-                    place.tags.delete(tag_previous)
-                end
+                find_and_delete_tag_from_place(place, previous)
             end
         end
         
         options[:current].split(" ").each do |current|
-            tag_current = Tag.all.find { |tag| tag.name == current } || Tag.new
-            if tag_current.id.nil?
-                tag_current.name = current
-                tag_current.weight = weight
-                tag_current.searched = 0
-            end
-            tag_current.save
-            place.tags << tag_current
+            find_and_add_tag_to_place(place, current, weight)
         end
     end
 end
